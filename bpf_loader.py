f97497496cfc9d77c28f33619336b45e59309ff5
securechicken <48861221+securechicken@users.noreply.github.com>
2022-06-24 21:12:07 +0200

    Update loader for Python 3.7+ IDA 7.4+

--- bpf_loader.py
+++ bpf_loader.py
@@ -1,97 +1,82 @@
-import idaapi
-from idc import *
+import ida_pro, ida_idaapi
+import ida_enum, ida_idp, ida_segment, ida_loader, ida_entry, ida_lines, ida_auto
 
+DEFAULT_BPF_FILE_MAGIC = b"bpf\x00"
+DEFAULT_BPF_FILE_MAGIC_LEN = len(DEFAULT_BPF_FILE_MAGIC)
+DEFAULT_BPF_FILE_RESERVED = b"\x00" * 4
+DEFAULT_BPF_FILE_RESERVED_LEN = len(DEFAULT_BPF_FILE_RESERVED)
+DEFAULT_BPF_FILE_HEADER_LEN = DEFAULT_BPF_FILE_MAGIC_LEN + DEFAULT_BPF_FILE_RESERVED_LEN
 
-def read_whole_file(li, s=0):
-    li.seek(s)
-    return li.read(li.size())
+def read_whole_file(li, s=DEFAULT_BPF_FILE_HEADER_LEN):
+	li.seek(s)
+	return li.read(li.size())
 
 def accept_file(li, n):
-    # we support only one format per file
-      
-    if idaapi.IDA_SDK_VERSION < 700 and n > 0:
-        return 0
-
-    li.seek(0)
-    if li.read(4) != 'bpf\0':
-        return 0
-
-    return {'format': "BPF file", 'options': 1|0x8000} # accept the file
-
-# -----------------------------------------------------------------------
+	# We support only one format per file
+	if ida_pro.IDA_SDK_VERSION < 700 and n > 0:
+		return 0
+	li.seek(0)
+	if li.read(DEFAULT_BPF_FILE_MAGIC_LEN) != DEFAULT_BPF_FILE_MAGIC:
+		return 0
+	return {"format": "BPF file", "options": 1|0x8000} # accept the file
 
 def setup_enums():
-    
-    enums = {'SECCOMP': [  #http://lxr.free-electrons.com/source/include/uapi/linux/seccomp.h#L28
-        ('SECCOMP_RET_KILL', 0, 'kill the task immediately'),
-        ('SECCOMP_RET_TRAP',  0x00030000, 'disallow and force a SIGSYS'),
-        ('SECCOMP_RET_ERRNO', 0x00050000, 'returns an errno'),
-        ('SECCOMP_RET_TRACE', 0x7ff00000, 'pass to a tracer or disallow'),
-        ('SECCOMP_RET_ALLOW', 0x7fff0000, 'allow')
-    ],
-    'AUDIT_ARCH':[
-        ('AUDIT_ARCH_I386', 0x40000003, ''),
-        ('AUDIT_ARCH_X86_64', 0xC000003E, '')
-    ]}
-
-    for enum_name in enums:
-        enum_vals = enums[enum_name]
-        repeat_cmnt = 0
-        enum_id = AddEnum(-1, enum_name, 0) # index -1 to append to list; bitmask 0
-
-        if enum_id == BADADDR:
-            print 'Unable to create enum SECCOMP'
-            return -1
-
-        for n,v,c in enum_vals:
-            if AddConst(enum_id, n, v):
-                print 'Unable to create {}'.format(n)
-                return -1
-            
-            if c:
-                const_id = GetConstByName(n)
-                if const_id == -1:
-                    print 'Unable to get id for {}'.format(n)
-                    return -1
-
-                
-                if not SetConstCmt(const_id, c, repeat_cmnt):
-                    print 'failed setting comment for {}'.format(n)
-                    return -1
-
-    print 'Finished creating enum'
+	enums = {"SECCOMP": [
+		# http://lxr.free-electrons.com/source/include/uapi/linux/seccomp.h#L28
+		("SECCOMP_RET_KILL", 0, "kill the task immediately"),
+		("SECCOMP_RET_TRAP",  0x00030000, "disallow and force a SIGSYS"),
+		("SECCOMP_RET_ERRNO", 0x00050000, "returns an errno"),
+		("SECCOMP_RET_TRACE", 0x7ff00000, "pass to a tracer or disallow"),
+		("SECCOMP_RET_ALLOW", 0x7fff0000, "allow")
+	],
+	"AUDIT_ARCH":[
+		("AUDIT_ARCH_I386", 0x40000003, ""),
+		("AUDIT_ARCH_X86_64", 0xC000003E, "")
+	]}
+	for enum_name in enums:
+		enum_vals = enums[enum_name]
+		repeat_cmnt = 0
+		# Index ida_idaapi.BADADDR to append to list; bitmask 0
+		enum_id = ida_enum.add_enum(ida_idaapi.BADADDR, enum_name, 0)
+		if enum_id == ida_idaapi.BADADDR:
+			print("Unable to create enum SECCOMP")
+			return -1
+		for n,v,c in enum_vals:
+			if ida_enum.add_enum_member(enum_id, n, v, -1):
+				print(f"Unable to create {n}")
+				return -1
+			if c:
+				const_id = ida_enum.get_enum_member_by_name(n)
+				if const_id == -1:
+					print(f"Unable to get id for {n}")
+					return -1
+				if not ida_enum.set_enum_member_cmt(const_id, c, repeat_cmnt):
+					print(f"Failed setting comment for {n}")
+					return -1
+	print("Finished creating enum")
 
 def load_file(li, neflags, format):
-    
-    # Select the PC processor module
-    idaapi.set_processor_type("BPF", SETPROC_ALL|SETPROC_FATAL)
-    
-    buf = read_whole_file(li, 8)
-    if not buf:
-        return 0
-
-    start = 0x0
-    seg = idaapi.segment_t()
-    size = len(buf)
-    end  = start + size
-    
-    # Create the segment
-    seg.startEA = start
-    seg.endEA   = end
-    seg.bitness = 1 # 32-bit
-    idaapi.add_segm_ex(seg, "bpf_c", "CODE", 0)
-
-    # Copy the bytes
-    idaapi.mem2base(buf, start, end)
-
-    # add entry point
-    idaapi.add_entry(start, start, "start", 1) 
-
-    # add comment to beginning of disassembly
-    idaapi.describe(start, True, "BPF bytecode disassembly")
-
-    # Mark for analysis
-    AutoMark(start, AU_CODE)
-
-    setup_enums()
-    return 1
+	# Select the PC processor module
+	ida_idp.set_processor_type("BPF", ida_idp.SETPROC_LOADER_NON_FATAL|ida_idp.SETPROC_LOADER)
+	buf = read_whole_file(li)
+	if not buf:
+		return 0
+	start = 0x0
+	seg = ida_segment.segment_t()
+	size = len(buf)
+	end = start + size
+	# Create the segment
+	seg.start_ea = start
+	seg.end_ea = end
+	seg.bitness = 1 # 32-bit
+	ida_segment.add_segm_ex(seg, "bpf_c", "CODE", 0)
+	# Copy the bytes
+	ida_loader.mem2base(buf, start, end)
+	# Add entry point
+	ida_entry.add_entry(start, start, "start", 1) 
+	# Add comment to beginning of disassembly
+	ida_lines.add_extra_line(start, True, "BPF bytecode disassembly")
+	# Mark for analysis
+	ida_auto.auto_mark(start, ida_auto.AU_CODE)
+	setup_enums()
+	return 1
